a52780c13ac4179308f2ddf6f2c0ea4e82fe9062
Shubham Arya <shubhamsunita7@gmail.com>
2026-08-18 15:02:34 +0530

    Add data types practice

---
+++ 03_data_types.py
@@ -0,0 +1,25 @@
+name = "Shubham"
+age = 19
+height = 5.8
+is_learning_python = True
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(height)
+print(type(height))
+
+print(is_learning_python)
+print(type(is_learning_python))
+
+"Germany"
+print(type("Germany"))
+2027
+print(type(2027))
+3.14
+print(type(3.14))
+False
+print(type(False))
