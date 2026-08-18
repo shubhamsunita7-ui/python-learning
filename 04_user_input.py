d15bd20e59b9ef83cd25fe78c2e0b227ab64bf6e
Shubham Arya <shubhamsunita7@gmail.com>
2026-08-18 15:33:17 +0530

    Add user input practice

---
+++ 04_user_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+country = input("Which country do you want to work in? ")
+
+print("Hello", name)
+print("You are", age, "years old")
+print("Your target country is", country)
